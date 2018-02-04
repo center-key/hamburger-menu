40ce7911022e775b89ae83cea4f3eb2810af9995
Dem Pilafian <dem@centerkey.com>
2018-02-04 11:00:04 -0800

    Test pages with link navigation moved into multi-page folder

--- spec/app.js
+++ spec/app.js
@@ -8,6 +8,8 @@ var app = {
    setup: function() {
       function makeIcon(i, elem) { $(elem).addClass('fa fa-' + $(elem).data().icon); }
       $('i[data-icon]').each(makeIcon);
+      // if (/single-page-app/.test(window.location.pathname))
+        // $('nav.hamburger-menu aside ul li').on({ click: app.actionClick })
       }
    };
 
