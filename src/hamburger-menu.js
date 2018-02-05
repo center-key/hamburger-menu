dba93b3290f42647087fe7d6b71397c4b2597767
Dem Pilafian <dem@centerkey.com>
2018-02-04 22:49:03 -0800

    Auto highlight current menu item on single-page apps

--- src/hamburger-menu.js
+++ src/hamburger-menu.js
@@ -1,19 +1,31 @@
 var hamburgerMenu = {
+   selectItem: function(event) {
+      var item = $(event.target).closest('li');
+      item.closest('aside').find('li').removeClass('current');
+      item.addClass('current');
+      },
    setup: function() {
       $(window.document).on({ click: $.noop });  //workaround for sticky hover on mobile
-      var current;
-      function isCurrent(i, elem) {
-         var linkUrl = new URL($(elem).attr('href'), current.url);
-         return linkUrl.pathname.replace(/[/]$/, '') === current.path;
-         }
-      function autoHighlight() {
-         current = {
+      var nav = $('nav.hamburger-menu');
+      function autoHighlightMultiPage() {
+         var current = {
             url: new URL(window.location.href),
             path: window.location.pathname.replace(/[/]$/, '')
             };
-         $('nav.hamburger-menu li >a').filter(isCurrent).first().closest('li').addClass('current');
+         function isCurrent(i, elem) {
+            var linkUrl = new URL($(elem).attr('href'), current.url);
+            return linkUrl.pathname.replace(/[/]$/, '') === current.path;
+            }
+         nav.find('li >a').filter(isCurrent).first().closest('li').addClass('current');
          }
-      if (!$('nav.hamburger-menu aside').hasClass('disable-auto-highlight'))
+      function autoHighlightSinglePageApp() {
+         nav.find('>aside li').on({ click: hamburgerMenu.selectItem });
+         }
+      function autoHighlight() {
+         autoHighlightMultiPage();
+         autoHighlightSinglePageApp();
+         }
+      if (!nav.find('>aside').hasClass('disable-auto-highlight'))
          autoHighlight();
       }
    };
