09b6f0f369278b5829cc963f2be85bbf6bcf38a0
Dem Pilafian <dem@centerkey.com>
2018-11-15 22:58:47 -0800

    Single task object for better organization

--- gulpfile.js
+++ gulpfile.js
@@ -21,8 +21,8 @@ const htmlHintConfig = { 'attr-value-double-quotes': false };
 const jsHintConfig = { strict: 'implied', undef: true, unused: true, browser: true, jquery: true };
 
 // Tasks
-const analyze = {
-   html: function() {
+const task = {
+   lintHtml: function() {
       return gulp.src('spec/**/*.html')
          .pipe(htmlHint(htmlHintConfig))
          .pipe(htmlHint.reporter())
@@ -30,18 +30,13 @@ const analyze = {
          .pipe(htmlValidator.reporter())
          .pipe(size({ showFiles: true }));
       },
-   js: function() {
+   lintJs: function() {
       return gulp.src('src/hamburger-menu.js')
          .pipe(jsHint(jsHintConfig))
          .pipe(jsHint.reporter())
          .pipe(size({ showFiles: true }));
       },
-   all: function() {
-      return mergeStream(analyze.html(), analyze.js());
-      }
-   };
-const minify = {
-   css: function() {
+   minify: function() {
       return mergeStream(
           gulp.src('src/hamburger-menu.css')
             .pipe(header(banner + '\n'))
@@ -50,23 +45,19 @@ const minify = {
             .pipe(rename('hamburger-menu.min.css'))
             .pipe(postCss([cssNano()]))
             .pipe(header(banner))
+            .pipe(gulp.dest('dist')),
+         gulp.src('src/hamburger-menu.js')
+            .pipe(header(banner + '\n'))
+            .pipe(gulp.dest('dist'))
+            .pipe(rename('hamburger-menu.min.js'))
+            .pipe(uglify())
+            .pipe(header(banner))
             .pipe(gulp.dest('dist'))
          );
-      },
-   js: function() {
-      return gulp.src('src/hamburger-menu.js')
-         .pipe(header(banner + '\n'))
-         .pipe(gulp.dest('dist'))
-         .pipe(rename('hamburger-menu.min.js'))
-         .pipe(uglify())
-         .pipe(header(banner))
-         .pipe(gulp.dest('dist'));
-      },
-   all: function() {
-      return mergeStream(minify.css(), minify.js());
       }
    };
 
 // Gulp
-gulp.task('lint',  analyze.all);
-gulp.task('build', minify.all);
+gulp.task('lint-html', task.lintHtml);
+gulp.task('lint-js',   task.lintHtml);
+gulp.task('minify',    task.minify);
