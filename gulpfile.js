90c6d4e4e600b66f39c7f3c10bd79677112a5376
Dem Pilafian <dem@centerkey.com>
2018-12-05 03:30:10 -0800

    Switch to babel to get es6 support

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-// hamburger-menu
+// HamburgerMenu ~ MIT License
+// Gulp configuration and tasks
 
 // Imports
 const cssNano =       require('cssnano');
+const babel =         require('gulp-babel');
+const gap =           require('gulp-append-prepend');
 const gulp =          require('gulp');
 const header =        require('gulp-header');
 const htmlHint =      require('gulp-htmlhint');
@@ -10,19 +13,24 @@ const jsHint =        require('gulp-jshint');
 const mergeStream =   require('merge-stream');
 const postCss =       require('gulp-postcss');
 const rename =        require("gulp-rename");
+const replace =       require('gulp-replace');
 const size =          require("gulp-size");
-const uglify =        require('gulp-uglify');
 
 // Setup
-const pkg = require('./package.json');
-const banner = '/*! HamburgerMenu v' + pkg.version +
-   ' ☰ github.com/center-key/hamburger-menu ☰ MIT License */\n';
-const htmlHintConfig = { 'attr-value-double-quotes': false };
 const jsHintConfig = { strict: 'implied', undef: true, unused: true, browser: true, jquery: true };
+const pkg =            require('./package.json');
+const home =           pkg.homepage.replace('https://', '');
+const banner =         'HamburgerMenu v' + pkg.version + ' ☰ ' + home + ' ☰ MIT License';
+const bannerCss =      '/*! ' + banner + ' */\n';
+const bannerJs =       '//! ' + banner + '\n';
+const htmlHintConfig = { 'attr-value-double-quotes': false };
+const headerComments = /^[/][/].*\n/gm;
+const transpileES6 =   ['@babel/env', { modules: false }];
+const babelMinifyJs =  { presets: [transpileES6, 'minify'], comments: false };
 
 // Tasks
 const task = {
-   lintHtml: function() {
+   analyzeHtml: () => {
       return gulp.src('spec/**/*.html')
          .pipe(htmlHint(htmlHintConfig))
          .pipe(htmlHint.reporter())
@@ -30,34 +38,39 @@ const task = {
          .pipe(htmlValidator.reporter())
          .pipe(size({ showFiles: true }));
       },
-   lintJs: function() {
+   lintJs: () => {
       return gulp.src('src/hamburger-menu.js')
          .pipe(jsHint(jsHintConfig))
          .pipe(jsHint.reporter())
          .pipe(size({ showFiles: true }));
       },
-   minify: function() {
-      return mergeStream(
-          gulp.src('src/hamburger-menu.css')
-            .pipe(header(banner + '\n'))
-            .pipe(gulp.dest('dist')),
-          gulp.src('src/hamburger-menu.css')
-            .pipe(rename('hamburger-menu.min.css'))
+   buildDistribution: () => {
+      const buildCss = () =>
+         gulp.src('src/hamburger-menu.css')
+            .pipe(header(bannerCss))
+            .pipe(gulp.dest('dist'))
             .pipe(postCss([cssNano()]))
-            .pipe(header(banner))
-            .pipe(gulp.dest('dist')),
+            .pipe(rename({ extname: '.min.css' }))
+            .pipe(size({ showFiles: true }))
+            .pipe(gulp.dest('dist'));
+      const buildJs = () =>
          gulp.src('src/hamburger-menu.js')
-            .pipe(header(banner + '\n'))
+            .pipe(replace(headerComments, ''))
+            .pipe(header(bannerJs))
+            .pipe(replace('[VERSION]', pkg.version))
+            .pipe(size({ showFiles: true }))
             .pipe(gulp.dest('dist'))
-            .pipe(rename('hamburger-menu.min.js'))
-            .pipe(uglify())
-            .pipe(header(banner))
-            .pipe(gulp.dest('dist'))
-         );
+            .pipe(babel(babelMinifyJs))
+            .pipe(rename({ extname: '.min.js' }))
+            .pipe(header(bannerJs))
+            .pipe(gap.appendText('\n'))
+            .pipe(size({ showFiles: true }))
+            .pipe(gulp.dest('dist'));
+      return mergeStream(buildCss(), buildJs());
       }
    };
 
 // Gulp
-gulp.task('lint-html', task.lintHtml);
-gulp.task('lint-js',   task.lintHtml);
-gulp.task('minify',    task.minify);
+gulp.task('lint-html',  task.analyzeHtml);
+gulp.task('lint-js',    task.lintJs);
+gulp.task('build-dist', task.buildDistribution);
