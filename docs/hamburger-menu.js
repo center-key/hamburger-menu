15197c75e4643b5925a6b95c77db4de53f10f301
Dem Pilafian <dem@centerkey.com>
2019-01-23 20:50:57 -0800

    Fix missing resources for spec pages

---
+++ docs/hamburger-menu.js
@@ -0,0 +1,46 @@
+//! HamburgerMenu v0.3.2 ☰ github.com/center-key/hamburger-menu ☰ MIT License
+
+const hamburgerMenu = {
+   version: '0.3.2',
+   selectItem: (event) => {
+      const item = $(event.target).closest('li');
+      item.closest('aside').find('li').removeClass('current');
+      item.addClass('current');
+      const nav = item.closest('.hamburger-menu').addClass('collapse-menu');
+      const eventRoutes = {};
+      const restoreAllowExand = () => {
+         nav.removeClass('collapse-menu');
+         console.log('restoreAllowExand');
+         $(window.document).off(eventRoutes);
+         };
+      eventRoutes.click =     restoreAllowExand;
+      eventRoutes.mousemove = restoreAllowExand;
+      const afterCurrentClick = 100;
+      window.setTimeout(() => $(window.document).on(eventRoutes), afterCurrentClick);
+      },
+   setup: () => {
+      $(window.document).on({ click: $.noop });  //workaround for sticky hover on mobile
+      const nav = $('nav.hamburger-menu');
+      const autoHighlightMultiPage = () => {
+         const current = {
+            url:  new window.URL(window.location.href),
+            path: window.location.pathname.replace(/[/]$/, '')
+            };
+         const isCurrent = (i, elem) => {
+            const linkUrl = new window.URL($(elem).attr('href'), current.url);
+            return linkUrl.pathname.replace(/[/]$/, '') === current.path;
+            };
+         nav.find('li >a').filter(isCurrent).first().closest('li').addClass('current');
+         };
+      const autoHighlightSinglePageApp = () =>
+         nav.find('>aside li').on({ click: hamburgerMenu.selectItem });
+      const autoHighlight = () => {
+         autoHighlightMultiPage();
+         autoHighlightSinglePageApp();
+         };
+      if (!nav.find('>aside').hasClass('disable-auto-highlight'))
+         autoHighlight();
+      }
+   };
+
+$(hamburgerMenu.setup);
