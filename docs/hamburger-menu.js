87bd73d1829e02c92c427bcc918da1fd4fa09d59
Dem Pilafian <dem@centerkey.com>
2019-01-24 13:37:53 -0800

    Release v0.3.3

--- docs/hamburger-menu.js
+++ docs/hamburger-menu.js
@@ -1,7 +1,7 @@
-//! HamburgerMenu v0.3.2 ☰ github.com/center-key/hamburger-menu ☰ MIT License
+//! HamburgerMenu v0.3.3 ☰ github.com/center-key/hamburger-menu ☰ MIT License
 
 const hamburgerMenu = {
-   version: '0.3.2',
+   version: '0.3.3',
    selectItem: (event) => {
       const item = $(event.target).closest('li');
       item.closest('aside').find('li').removeClass('current');
