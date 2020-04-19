9f96ad8e185a5b820c6f83be4e7992f76976db32
Dem Pilafian <dem@centerkey.com>
2020-04-19 13:24:22 -0700

    Switch to es6 object function notation

--- src/hamburger-menu.js
+++ src/hamburger-menu.js
@@ -2,7 +2,7 @@
 
 const hamburgerMenu = {
    version: '[VERSION]',
-   selectItem: (event) => {
+   selectItem(event) {
       const item = $(event.target).closest('li');
       item.closest('aside').find('li').removeClass('current');
       item.addClass('current');
@@ -18,13 +18,13 @@ const hamburgerMenu = {
       const afterCurrentClick = 100;
       window.setTimeout(() => $(window.document).on(eventRoutes), afterCurrentClick);
       },
-   setup: () => {
+   setup() {
       $(window.document).on({ click: $.noop });  //workaround for sticky hover on mobile
       const nav = $('nav.hamburger-menu');
       const autoHighlightMultiPage = () => {
          const current = {
             url:  new window.URL(window.location.href),
-            path: window.location.pathname.replace(/\/$/, '')
+            path: window.location.pathname.replace(/\/$/, ''),
             };
          const isCurrent = (i, elem) => {
             const linkUrl = new window.URL($(elem).attr('href'), current.url);
@@ -40,7 +40,7 @@ const hamburgerMenu = {
          };
       if (!nav.find('>aside').hasClass('disable-auto-highlight'))
          autoHighlight();
-      }
+      },
    };
 
 $(hamburgerMenu.setup);
