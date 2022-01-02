f68c6c031f5cb453b06dd6996497153426ea5c21
dpilafian <dem@centerkey.com>
2022-01-02 12:45:00 -0800

    Release v0.4.0

--- dist/hamburger-menu.js
+++ dist/hamburger-menu.js
@@ -1,8 +1,8 @@
-//! hamburger-menu v0.3.9 ☰ https://github.com/center-key/hamburger-menu ☰ MIT License
+//! hamburger-menu v0.4.0 ☰ https://github.com/center-key/hamburger-menu ☰ MIT License
 
 const hamburgerMenu = {
 
-   version: '0.3.9',
+   version: '0.4.0',
 
    selectItem(event) {
       const item = $(event.target).closest('li');
