4ed9897272d241c3e23a29736e454f1c2ed08a14
dpilafian <dem@centerkey.com>
2022-10-03 14:57:45 -0700

    Simplify build steps with uglifyjs

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,12 @@
 // Gulp configuration and tasks
 
 // Imports
-import babel   from 'gulp-babel';
 import cssNano from 'cssnano';
 import gap     from 'gulp-append-prepend';
 import gulp    from 'gulp';
 import postCss from 'gulp-postcss';
 import rename  from 'gulp-rename';
 import size    from 'gulp-size';
-
-// Setup
-const transpileES6 =  ['@babel/env', { modules: false }];
-const babelMinifyJs = { presets: [transpileES6, 'minify'], comments: false };
 
 // Tasks
 const task = {
@@ -29,17 +24,7 @@ const task = {
          .pipe(gulp.dest('build'));
       },
 
-   minifyJs() {
-      return gulp.src('src/hamburger-menu.js')
-         .pipe(babel(babelMinifyJs))
-         .pipe(rename({ extname: '.min.js' }))
-         .pipe(gap.appendText('\n'))
-         .pipe(size({ showFiles: true }))
-         .pipe(gulp.dest('build'));
-      },
-
    };
 
 // Gulp
 gulp.task('minify-css', task.minifyCss);
-gulp.task('minify-js',  task.minifyJs);
